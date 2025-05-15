94d958e0b9f165a3e2330bb2dd52e303adfb019f
Ahmad Bhatti <vascobhae@gmail.com>
2025-05-15 21:30:51 +0500

    Remove comments

--- 1_Robospeaker.py
+++ 1_Robospeaker.py
@@ -1,8 +1,3 @@
-#pip install pyttsx3 (run this command in your terminal to install the library)
-# This is a simple text-to-speech program.
-# It prompts the user for input and speaks the text aloud.
-# This script uses the pyttsx3 library to convert text to speech.
-
 import pyttsx3
 
 # Initialize the text-to-speech engine
